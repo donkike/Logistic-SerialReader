9cd77693b1397ab9ae11c5860e93ad722f0e5209
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-22 20:15:52 +0200

    no ids needed for update and added http to host

--- serial.rb
+++ serial.rb
@@ -18,29 +18,30 @@ sp = SerialPort.new(port, baud_rate, data_bits, stop_bits, parity)
 users = {}
 
 loop do
-  #puts sp.gets('*')
-  puts "Receiving..."
-  id = sp.gets('*').gsub('*', '').to_i
-  users[id] ||= {}
-  activity = sp.gets('*').gsub('*', '').to_i
-  if users[id][activity].nil? # register activity
-    users[id][activity] = {:start => Time.now}
-    server_ans =  Server.post("#{host}/report_activity_users.xml", :body => {
-                                :report_activity_user => {:user_id => id, :activity_id => activity
-                                }})
-    puts server_ans.parsed_response
-    puts users[id][activity][:id] = server_ans.parsed_response['report_activity_user']['id']
-  #else
-  end
-    repetitions = sp.gets('*')    
-    users[id][activity][:reps] = repetitions
-  puts "Repetitions #{repetitions}"
-    thisID = users[id][activity][:id]
-    server_ans = Server.put("#{host}/report_activity_users/#{thisID}.xml", :body => {:report_activity_user => {
-                  :user_id => id, :activity_id => activity, :real_time => (repetitions.to_f / (Time.now - users[id][activity][:start]))
-                               }})
-    puts server_ans.parsed_response
-  #end
+  puts sp.gets('*')
+  # puts "Receiving..."
+  # id = sp.gets('*').gsub('*', '').to_i
+  # users[id] ||= {}
+  # activity = sp.gets('*').gsub('*', '').to_i
+  # if users[id][activity].nil? # register activity
+  #   users[id][activity] = {:start => Time.now}
+  #   server_ans =  Server.post("http://#{host}/report_activity_users.xml", :body => {
+  #                               :report_activity_user => {
+  #                                 :user_id => id, :activity_id => activity
+  #                               }})
+  #   puts server_ans.parsed_response
+  #   puts users[id][activity][:id] = server_ans.parsed_response['report_activity_user']['id']
+  # else  
+  #   repetitions = sp.gets('*').gsub('*', '').to_i    
+  #   users[id][activity][:reps] = repetitions
+  #   puts "Repetitions #{repetitions}"
+  #   thisID = users[id][activity][:id]
+  #   server_ans = Server.put("http://#{host}/report_activity_users/#{thisID}.xml", :body => {
+  #                             :report_activity_user => {
+  #                               :real_time => (repetitions.to_f / (Time.now - users[id][activity][:start]))
+  #                              }})
+  #   puts server_ans.parsed_response
+  # end
   #printf("%c\n", sp.getc)
 end
 
